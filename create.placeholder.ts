dc95bf9d00048b6ff337c7723d02c8822941ea6f
PrateekUpd <prateek.upadhyay31@gmail.com>
2020-06-25 17:17:37 +0530

    first commit

---
+++ create.placeholder.ts
@@ -0,0 +1,86 @@
+[
+    {
+      modeOfTransport: '',
+      description: '',
+      quantity: 99,
+      unit: '',
+      issuedTo: '',
+      dateOfReturn: new Date(2017, 4, 4),
+      from: '',
+      to: '',
+      reason: '',
+      remark: '',
+      incomingRef: ''
+    },
+    {
+        modeOfTransport: '',
+        description: '',
+        quantity: 99,
+        unit: '',
+        issuedTo: '',
+        dateOfReturn: new Date(2017, 4, 4),
+        from: '',
+        to: '',
+        reason: '',
+        remark: '',
+        incomingRef: ''
+      },
+    
+      {
+        modeOfTransport: '',
+        description: '',
+        quantity: 99,
+        unit: '',
+        issuedTo: '',
+        dateOfReturn: new Date(2017, 4, 4),
+        from: '',
+        to: '',
+        reason: '',
+        remark: '',
+        incomingRef: ''
+      },
+    
+      {
+        modeOfTransport: '',
+        description: '',
+        quantity: 99,
+        unit: '',
+        issuedTo: '',
+        dateOfReturn: new Date(2017, 4, 4),
+        from: '',
+        to: '',
+        reason: '',
+        remark: '',
+        incomingRef: ''
+      },
+    
+      {
+        modeOfTransport: '',
+        description: '',
+        quantity: 99,
+        unit: '',
+        issuedTo: '',
+        dateOfReturn: new Date(2017, 4, 4),
+        from: '',
+        to: '',
+        reason: '',
+        remark: '',
+        incomingRef: ''
+      },
+  
+      {
+        modeOfTransport: '',
+        description: '',
+        quantity: 99,
+        unit: '',
+        issuedTo: '',
+        dateOfReturn: new Date(2017, 4, 4),
+        from: '',
+        to: '',
+        reason: '',
+        remark: '',
+        incomingRef: ''
+      },
+    
+  
+  ]
